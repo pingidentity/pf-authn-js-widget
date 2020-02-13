ef00a8cf76043358964712c54d48eb2535422434
Philip Coldrey <philipcoldrey@pingidentity.com>
2020-02-13 14:12:02 -0800

    Use CDN for Ping default logo
    What: previously the logo was retrieved from the local directory and some
    applications had path issues.
    How: if a logo is not provided it is now retrieved from the CDN.
    Closes #9

--- src/utils/assets.js
+++ src/utils/assets.js
@@ -3,7 +3,7 @@ import pinglogo from '../assets/ping-logo.svg'
 export default class Assets {
 
   constructor(options) {
-    this.logo = (options && options.logo) || pinglogo;
+    this.logo = (options && options.logo) || 'https://assets.pingone.com/ux/end-user/0.14.0/images/ping-logo.svg';
     this.error = (options && options.error) || 'https://assets.pingone.com/ux/end-user/0.13.0/icons/error.svg';
     this.spinner = (options && options.spinner) || 'https://assets.pingone.com/ux/end-user/0.13.0/icons/spinner.svg'
   }
