0f1cb56e3fdf69716b161e6f6cc7c7d827d710c9
philipcoldrey-ping <58954944+philipcoldrey-ping@users.noreply.github.com>
2020-12-17 17:21:59 +0000

    Action query param (#45)
    * Adding the ability to request actions via a query parameter instead of
    by custom content type.
    
    * fixed failing unit tests.
    
    Co-authored-by: Nick Sawadsky <nsawadsky@pingidentity.com>
    Co-authored-by: Jamshid Khosravian <khosravian@gmail.com>

--- src/utils/fetchUtil.js
+++ src/utils/fetchUtil.js
@@ -1,33 +1,40 @@
 export default class fetchUtil {
-  constructor(baseUrl) {
+  constructor(baseUrl, useActionParam=false) {
     this.baseUrl = baseUrl;
+    this.useActionParam = useActionParam;
   }
 
-  doRequest(method, flowId, contentType, body) {
+  doRequest(method, flowId, actionId, body) {
     var FLOWS_ENDPOINT = '/pf-ws/authn/flows/';
+    var url = this.baseUrl + FLOWS_ENDPOINT + flowId;
     var headers = {
       Accept: 'application/json',
-      'X-XSRF-Header': 'PingFederate',
-      'Content-Type': contentType
+      'X-XSRF-Header': 'PingFederate'
     };
-
+    if (actionId) {
+      var contentType = 'application/json';
+      if (this.useActionParam) {
+        url = url + '?action=' + actionId;
+      }
+      else {
+        contentType = 'application/vnd.pingidentity.' + actionId + '+json';
+      }
+      headers['Content-Type'] = contentType;
+    }
     var options = {
       headers: headers,
       method: method,
       body: body,
       credentials: 'include'
     }
-    var url = this.baseUrl + FLOWS_ENDPOINT + flowId;
     return fetch(url, options);
   }
-
 
   getFlow(flowId) {
     return this.doRequest('GET', flowId);
   }
 
   postFlow(flowId, actionId, body) {
-    var customContentType = 'application/vnd.pingidentity.' + actionId + '+json';
-    return this.doRequest('POST', flowId, customContentType, body);
+    return this.doRequest('POST', flowId, actionId, body);
   }
 }
