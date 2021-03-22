d0c7d24e8c26fe3e420e1ac3af6ed3b349ffd35d
Jonathan Wong <jonathanwong@pingidentity.com>
2021-03-22 08:33:48 -0700

    revise focus

--- src/index.js
+++ src/index.js
@@ -114,6 +114,7 @@ export default class AuthnWidget {
     this.addEventHandler('USERNAME_PASSWORD_REQUIRED', this.registerAltAuthSourceLinks);
     this.addEventHandler('REGISTRATION_REQUIRED', this.registerRegistrationLinks);
     this.addEventHandler('REGISTRATION_REQUIRED', this.registerAltAuthSourceLinks);
+    this.addPostRenderCallback('REGISTRATION_REQUIRED', this.focusFirstInput);
     this.addEventHandler('EXTERNAL_AUTHENTICATION_COMPLETED', this.postContinueAuthentication);
     this.addEventHandler('EXTERNAL_AUTHENTICATION_REQUIRED', this.registerReopenPopUpHandler);
     this.addPostRenderCallback('RESUME', this.resumeToPf);
@@ -126,6 +127,7 @@ export default class AuthnWidget {
     this.addEventHandler('DEVICE_SELECTION_REQUIRED', this.registerMfaEventHandler);
     this.addEventHandler('OTP_REQUIRED', this.registerMfaEventHandler);
     this.addEventHandler('OTP_REQUIRED', this.registerMfaChangeDeviceEventHandler);
+    this.addPostRenderCallback('OTP_REQUIRED', this.focusFirstInput);
 
     this.addEventHandler('ASSERTION_REQUIRED', this.registerMfaEventHandler);
     this.addEventHandler('ASSERTION_REQUIRED', this.registerMfaChangeDeviceEventHandler);
@@ -223,11 +225,6 @@ export default class AuthnWidget {
         dropdown.addEventListener('change', this.onChangeDropdown);
         this.checkDropdownSelected(dropdown);
       });
-    }
-
-    let firstTextInput = document.querySelector("input[type=text]:not(:disabled)");
-    if (firstTextInput) {
-      firstTextInput.focus();
     }
 
     let element = nodes[0];
@@ -930,6 +927,14 @@ export default class AuthnWidget {
       status.classList.remove('text-input__icon--success');
       status.classList.add('text-input__icon--error');
       document.querySelector('#submit').disabled = true;
+    }
+  }
+
+  focusFirstInput() {
+    let firstInput = document.querySelector("input[type=text]:not(:disabled), " +
+      "input[type=email]:not(:disabled), input[type=date]:not(:disabled), input[type=phone]:not(:disabled)");
+    if (firstInput) {
+      firstInput.focus();
     }
   }
 }
