c544d4b49d57da64a9b496b8fa61e5edb41da383
Felix Suen <felixsuen@pingidentity.com>
2021-07-27 16:28:03 -0700

    added more type checkings

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,12 +1,20 @@
 declare module "@ping-identity/pf-authn-js-widget"
 
+export interface RedirectlessConfig {
+  client_id: string,
+  response_type: string,
+  onAuthorizationSuccess(response: any): void,
+  scopes?: string[],
+  state?: string
+}
+
 export interface IAuthnWidget {
   init(): void;
   initRedirectless(redirectlessConfig: any): void
 }
 
 export default class AuthnWidget implements IAuthnWidget {
-  constructor(baseUrl: string, options: any);
+  constructor(baseUrl: string, options: Options);
   init(): void;
-  initRedirectless(redirectlessConfig: any): void
+  initRedirectless(redirectlessConfig: RedirectlessConfig): void
 }
