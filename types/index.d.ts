9e2135c1198cc134747ba694562670ad1ecce1e0
Felix Suen <felixsuen@pingidentity.com>
2021-07-30 15:59:17 -0700

    options definitions

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,20 +1,35 @@
 declare module "@ping-identity/pf-authn-js-widget"
 
-export interface RedirectlessConfig {
-  client_id: string,
-  response_type: string,
-  onAuthorizationSuccess(response: any): void,
+export interface IRedirectlessConfig {
+  client_id?: string,
+  response_type?: string,
+  onAuthorizationSuccess: (response: Response) => void,
+  onAuthorizationRequest?: () => Promise<Response>,
   scopes?: string[],
   state?: string
 }
 
+export interface IOptions {
+  divId: string,
+  flowId?: string,
+  logo?: string,
+  invokeReCaptcha?: () => void,
+  checkRecaptcha?: string,
+  grecaptcha?: any,
+  deviceProfileScript?: string
+}
+
 export interface IAuthnWidget {
   init(): void;
-  initRedirectless(redirectlessConfig: any): void
+  initRedirectless(redirectlessConfig: RedirectlessConfig): void;
+  dispatchPendingState(token: any): void;
+  clearPendingState(): void;
 }
 
 export default class AuthnWidget implements IAuthnWidget {
   constructor(baseUrl: string, options: Options);
   init(): void;
-  initRedirectless(redirectlessConfig: RedirectlessConfig): void
+  initRedirectless(redirectlessConfig: RedirectlessConfig): void;
+  dispatchPendingState(token: any): void;
+  clearPendingState(): void;
 }
