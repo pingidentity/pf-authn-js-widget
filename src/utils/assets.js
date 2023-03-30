98bd7365d2fef5bf194ee06b7063110006f9aaee
Shuoren Wang <shuorenwang@pingidentity.com>
2023-03-30 14:44:54 -0700

    Update Ping logo

--- src/utils/assets.js
+++ src/utils/assets.js
@@ -2,7 +2,7 @@
 export default class Assets {
 
   constructor(options) {
-    this.logo = (options && options.logo) || 'https://assets.pingone.com/ux/end-user/0.36.1/images/ping-logo.svg';
+    this.logo = (options && options.logo) || 'https://assets.pingone.com/ux/end-user/2.4.22/images/ping-logo.svg';
   }
 
   toTemplateParams() {
