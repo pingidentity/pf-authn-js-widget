d2a9add057280a50c8b7cf077c6597aab24b2ddf
Shuoren Wang <shuorenwang@pingidentity.com>
2023-04-13 14:01:32 -0700

    Use PingIdentity logo from UI library to replace the one from end-user lib

--- src/utils/assets.js
+++ src/utils/assets.js
@@ -2,7 +2,7 @@
 export default class Assets {
 
   constructor(options) {
-    this.logo = (options && options.logo) || 'https://assets.pingone.com/ux/end-user/2.4.22/images/ping-logo.svg';
+    this.logo = (options && options.logo) || 'https://assets.pingone.com/ux/ui-library/5.6.11/images/logo-pingidentity.png';
   }
 
   toTemplateParams() {
