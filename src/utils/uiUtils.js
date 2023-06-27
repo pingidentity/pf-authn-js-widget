6acfdda2f590ae2a7c48d211e07a420fd30c497f
Tomer Hadad <hadad.tomer@gmail.com>
2023-06-27 23:15:23 +0300

    CAPTCHA/Risk provider support (#127)
    CAPTCHA/Risk provider support
    
    - Signals SDK implementation

--- src/utils/uiUtils.js
+++ src/utils/uiUtils.js
@@ -1,10 +1,10 @@
 /**
  * Checks if a script is loaded into the DOM or not.
- * 
+ *
  * @param {String} name the part of the script name or path to look for.
  * @returns true if the script is already loaded into the DOM, false otherwise
  */
-const isScriptLoaded = (name) => {
+export const isScriptLoaded = (name) => {
   const scripts = document.getElementsByTagName('script');
   for (let i = 0; i < scripts.length; i++) {
     const script = scripts[i];
